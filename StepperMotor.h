6c53d8392b25b1233c353798fa4419bf33913b26
rtlessly <tlessly@hotmail.com>
2018-12-27 10:58:57 -0600

    Updated documentation

--- StepperMotor.h
+++ StepperMotor.h
@@ -1,26 +1,26 @@
-/******************************************************************
- 4-Wire interface for a stepper motor connected to an Arduino. This
- interface requires 4 digital pins on the Arduino. It is assumed that
- the motor is connected to the Arduino via a simple pass-thru power
- controller such as an ULN2003 or ULN2004 Darlington array. 
- 
- !!! NEVER CONNECT STEPPER MOTOR LEADS DIRECTLY TO THE ARDUINO!!! 
- 
- Written by R. Terry Lessly 2018-12-03.
- ******************************************************************/
 #ifndef _StepperMotor_h_
 #define _StepperMotor_h_
 
 #include <inttypes.h>
 #include <RTL_EventFramework.h>
-#include "IStepperMotor.h"
+// #include "IStepperMotor.h"
 
 
+//******************************************************************************
+/// 4-Wire interface for a stepper motor connected to an Arduino. This interface 
+/// requires 4 digital pins on the Arduino. It is assumed that the motor is
+/// connected to the Arduino via a simple pass-thru power controller such as an
+/// ULN2003 or ULN2004 Darlington array. 
+///  
+///  !!! NEVER CONNECT STEPPER MOTOR LEADS DIRECTLY TO THE ARDUINO!!! 
+///  
+///  Written by R. Terry Lessly 2018-12-03.
+//******************************************************************************
 class StepperMotor : public IPollable
 {
     DECLARE_CLASSNAME;
 
-	//**************************************************************************
+    //**************************************************************************
     /// Stepper motor modes
     //**************************************************************************
     public: enum MotorMode
@@ -36,6 +36,13 @@ class StepperMotor : public IPollable
     /// NOTE: Directions are aribtrary, i.e., FORWARD just means one direction 
     ///       and BACKWARD means the opposite direction. The actual direction the 
     ///       motor turns depends on how the motor is wired up to the Arduino. 
+	///
+    ///       If the motor is running backwards from the desired direction you 
+    ///       can either:
+    ///       1) Swap FORWARD and BACKWARD directions in your code
+    ///       2) Swap the "A" leads and "B" leads on the motor (NOTE: DO NOT swap
+    ///          the "A" leads with the "B" leads, just switch A1 with A2 and 
+	///          B1 with B2).
     //**************************************************************************
     public: enum MotorDirection
     {
@@ -55,14 +62,11 @@ class StepperMotor : public IPollable
     public: void Poll();
 
 
-    /*--------------------------------------------------------------------------
-    IStepperMotor interface
-    --------------------------------------------------------------------------*/
     //**************************************************************************
     /// Starts the motor. The motor will not run until this method is called,
     /// and then only if the speed is non-zero. If the steps parameter is non-
-	/// zero then the motor will only run for the specified number of steps and
-	/// then stop. Otherwise, it runs indefinitely until Stop() is called.
+    /// zero then the motor will only run for the specified number of steps and
+    /// then stop. Otherwise, it runs indefinitely until Stop() is called.
     //**************************************************************************
     public: void Start(uint32_t steps=0) { _isRunning = (_speed != 0); if (steps > 0) _stepCount = steps; };
 
@@ -79,7 +83,7 @@ class StepperMotor : public IPollable
 
     //**************************************************************************
     /// Advances the motor exactly one step in the specified direction. 
-    /// This is the key method used by the StepperMotorController class.
+    /// This is the key method used operate the motor.
     //**************************************************************************
     public: void OneStep(int8_t dir);
 
@@ -108,11 +112,11 @@ class StepperMotor : public IPollable
 
     //**************************************************************************
     /// Gets the motor step count. The step count is only valid after the 
-	/// RunSteps() method has been called and only until the step count reaches 
-	/// zero. At all other times the step count is 0.
+    /// RunSteps() method has been called and only until the step count reaches 
+    /// zero. At all other times the step count is 0.
     ///
     /// \return The current motor step count. This is the number of steps until
-	///         the count reaches 0 rather than the number of steps taken so far.
+    ///         the count reaches 0 rather than the number of steps taken so far.
     //**************************************************************************
     public: inline uint32_t Position() { return _stepCount; };
 
@@ -136,14 +140,14 @@ class StepperMotor : public IPollable
     /*--------------------------------------------------------------------------
     Internal state (size: 20 bytes)
     --------------------------------------------------------------------------*/
-    private: struct             // 2 bytes
+    private: struct                     // 2 bytes
     {
-        uint16_t motorNum :  2; // The motor number (0 - 3)
-        uint16_t mode     :  2; // The motor mode
-        uint16_t pinA1    :  4; // Motor 'A' coil lead 1 Arduino pin
-        uint16_t pinA2    :  4; // Motor 'A' coil lead 2 Arduino pin
-        uint16_t pinB1    :  4; // Motor 'B' coil lead 1 Arduino pin
-        uint16_t pinB2    :  4; // Motor 'B' coil lead 2 Arduino pin
+        uint16_t motorNum :  2;         // The motor number (0 - 3)
+        uint16_t mode     :  2;         // The motor mode
+        uint16_t pinA1    :  4;         // Motor 'A' coil lead 1 Arduino pin
+        uint16_t pinA2    :  4;         // Motor 'A' coil lead 2 Arduino pin
+        uint16_t pinB1    :  4;         // Motor 'B' coil lead 1 Arduino pin
+        uint16_t pinB2    :  4;         // Motor 'B' coil lead 2 Arduino pin
     }
     _motorState;
 
